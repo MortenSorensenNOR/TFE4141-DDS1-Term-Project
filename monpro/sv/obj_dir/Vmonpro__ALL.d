49e73378333e2d52f05dfaead044dcd19133b267
Morten Sørensen <morten.sorensen.04@gmail.com>
2024-10-31 17:00:20 +0100

    working mon pro sv

--- monpro/sv/obj_dir/Vmonpro__ALL.d
+++
@@ -1,8 +0,0 @@
-Vmonpro__ALL.o: Vmonpro__ALL.cpp Vmonpro.cpp Vmonpro.h \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilatedos.h Vmonpro__Syms.h \
- Vmonpro___024unit.h Vmonpro___024unit.cpp Vmonpro__Trace.cpp \
- /usr/share/verilator/include/verilated_vcd_c.h \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilated_trace.h Vmonpro__Slow.cpp \
- Vmonpro___024unit__Slow.cpp Vmonpro__Syms.cpp Vmonpro__Trace__Slow.cpp
--- monpro/sv/obj_dir/Vmonpro___024unit.cpp
+++
@@ -1,8 +0,0 @@
-// Verilated -*- C++ -*-
-// DESCRIPTION: Verilator output: Design implementation internals
-// See Vmonpro.h for the primary calling header
-
-#include "Vmonpro___024unit.h"
-#include "Vmonpro__Syms.h"
-
-//==========
